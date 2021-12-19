3b3d827a135185b277530dc23c59b44f5ac3efd7
Sergei Perepelkin <perepelkin.serge@gmail.com>
2021-12-19 17:34:38 +0100

    Set up a database

---
+++ server/db.sql
@@ -0,0 +1,14 @@
+-- Create database
+CREATE DATABASE jwt;
+
+-- Install extension
+create extension if not exists "uuid-ossp"; 
+
+-- SCHEMA OF USERS
+CREATE TABLE users(
+    user_id uuid PRIMARY KEY DEFAULT
+    uuid_generate_v4(),
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL
+);
