0cbbb798026197a8ad69d613e56470cc3c5eae03
Sergei Perepelkin <perepelkin.serge@gmail.com>
2021-12-27 22:35:37 +0100

    Set up routes inside react-app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Redirect } from "react-router-dom"
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
